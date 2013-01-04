113ab44b42b45cb8b60c39aa34cc1ddc5558b3f2
Paul Tew <paul@paultew.com>
2013-01-04 22:32:48 +0000

    Moved build files

--- Build.ps1
+++ Build.ps1
@@ -1,13 +1,17 @@
-$package_version = "0.0.1"
+$package_version = "0.1.0.1"
 $base_dir = resolve-path .\
-$sln_file = "$base_dir\src\Elmah.Masking.sln"
+$src_dir = "$base_dir\src"
+$sln_file = "$src_dir\Elmah.Masking.sln"
 $package_file = "$base_dir\Elmah.Masking.nuspec"
+$version_exe = "$base_dir\build\UpdateVersion.exe"
 $nuget_exe = "C:\Tools\NuGet.exe"
 $msbuild_exe = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 $config = "Release"
 
 & "$msbuild_exe" /v:q /t:Clean $sln_file /p:Configuration=$config /nologo
- 
+
+& "$version_exe" -v Assembly -p "$package_version" -i "$src_dir\Common\AssemblyInfo.cs" -o "$src_dir\Common\AssemblyInfo.cs"
+
 & "$msbuild_exe" /v:m /t:Rebuild $sln_file /p:Configuration=$config /nologo
 
-& "$nuget_exe" pack $package_file -Version $package_version -OutputDirectory C:\Projects\NugetPackages\
+& "$nuget_exe" pack $package_file -verbosity detailed -Version $package_version -OutputDirectory C:\Projects\NugetPackages\
--- src/Common/AssemblyInfo.cs
+++ src/Common/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Elmah.Masking")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Elmah.Masking")]
-[assembly: AssemblyCopyright("Copyright © Paul Tew 2013")]
+[assembly: AssemblyCopyright("Copyright � Paul Tew 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDescription("")]
@@ -31,6 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
